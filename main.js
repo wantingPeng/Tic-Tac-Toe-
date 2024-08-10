26341a340a9fa170816db2f3485921ea540202eb
wanting <wantingpeng.cici@gmail>
2024-08-10 21:25:00 +0200

    fix the hover bug and {once:true}:unavoid
    , otherweise 'cell x o'

--- main.js
+++ main.js
@@ -4,15 +4,20 @@ const board =document.querySelector('[data-board]')
 console.log(cellALL)
 let flag=true
 cellALL.forEach(cell=>{
+  hover()
+  cell.addEventListener('click',addXorO,{once:true})
 
-  cell.addEventListener('click',addXorO)
 })
 function addXorO(event){
   mark=flag?'x':'o'
  event.target.classList.add(mark)
- board.classList.remove(mark)
+  board.classList.remove(mark) 
  flag=!flag
-console.log(flag)
-mark=flag?'x':'o'
+ hover()
+}
+
+function hover(){
+  mark=flag?'x':'o'
 board.classList.add(mark)
+
 }
