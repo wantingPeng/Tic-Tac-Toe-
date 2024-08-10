19e1836836d3ac771aad50d3bd693c8e838ead2d
wanting <wantingpeng.cici@gmail>
2024-08-10 20:16:46 +0200

    display hover mark but when it is not clicked at the beginning, there is no hover function, so the relevant hover function must be called before line 8

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
-//click=>show X,then click show O,switch the 
+//hover
 const cellALL = document.querySelectorAll('[data-cell]')
+const board =document.querySelector('[data-board]')
 console.log(cellALL)
 let flag=true
 cellALL.forEach(cell=>{
+
   cell.addEventListener('click',addXorO)
 })
 function addXorO(event){
   mark=flag?'x':'o'
  event.target.classList.add(mark)
+ board.classList.remove(mark)
  flag=!flag
 console.log(flag)
-
+mark=flag?'x':'o'
+board.classList.add(mark)
 }
